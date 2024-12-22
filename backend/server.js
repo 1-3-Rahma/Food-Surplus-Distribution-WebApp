ad0b0edec0f60d2707ae3b4822d4066d28486676
Salma Ahmed <salma.akafarag162@gmail.com>
2024-12-22 16:43:39 +0200

    Food donation

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require("./routes/authRoutes");
-<<<<<<< HEAD
 const foodDonationRoutes = require('./routes/foodDonationRoutes');
 // const notificationRoutes = require('./routes/notificationRoutes');
 const deleteExpiredDonations = require('./utils/JobScheduler');
-=======
-const notificationRoutes = require('./routes/notificationRoutes');
-// const deleteExpiredDonations = require('./utils/JobScheduler');
-// const foodDonationRoutes = require('./routes/foodDonationRoutes');
->>>>>>> 8b599d582bb05aa3833008a61e35b606a7e6076a
 
 const orderRoutes = require('./routes/OrderRoutes');
 
@@ -25,13 +19,8 @@ connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes);
-<<<<<<< HEAD
 app.use('/api/food', foodDonationRoutes);
 // app.use('/api/notifications', notificationRoutes);
-=======
-// app.use('/api/food', foodDonationRoutes);
-app.use('/api/notifications', notificationRoutes);
->>>>>>> 8b599d582bb05aa3833008a61e35b606a7e6076a
 app.use('/api/orders', orderRoutes);
 
 // Job Scheduler
